a6f96f5766d3396edbdb49d2be7249242ded9468
meer-khan <shahmirkhan519@gmail.com>
2025-03-06 04:09:34 +0500

    updated settings

--- project_management/project_management/settings.py
+++ project_management/project_management/settings.py
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framework_simplejwt.token_blacklist"
 ]
 
 REST_FRAMEWORK = {
